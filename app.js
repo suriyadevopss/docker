d83bcf7e987fa49bde03d4fd43d910ab97999784
Ss, Suriya <suriya.ss@dyson.com>
2024-01-29 10:42:00 +0530

    s3 list

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 80;
+const AWS = require('aws-sdk');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Create an S3 instance
+const s3 = new AWS.S3();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Function to list all S3 buckets
+function listAllBuckets() {
+  s3.listBuckets((err, data) => {
+    if (err) {
+      console.error('Error listing buckets:', err);
+    } else {
+      console.log('List of S3 buckets:');
+      data.Buckets.forEach((bucket) => {
+        console.log(bucket.Name);
+      });
+    }
+  });
+}
+
+// Function to periodically list all buckets
+function startListing() {
+  listAllBuckets();
+  setInterval(() => {
+    listAllBuckets();
+  }, 5 * 60 * 1000); // 5 minutes in milliseconds
+}
+
+// Start listing buckets
+startListing();
